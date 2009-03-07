6f5c5259b4fd405aa7bd839e86f1f2d976d1e2d4
Murray Steele <muz@h-lame.com>
2009-03-07 17:30:23 +0000

    Making the regexp against the write to them page more comprehensive.

--- lib/council.rb
+++ lib/council.rb
@@ -32,11 +32,11 @@ get '/council/:post_code' do
   
   doc = Hpricot(open("http://www.writetothem.com/who?pc=#{params[:post_code]}"))
   
-  council_col = (doc/"table#repstable tr:nth-of-type(0) th").index {|e| e.inner_html =~ /Your Councillors/}
+  council_col = (doc/"table#repstable tr:nth-of-type(0) th").index {|e| e.inner_html =~ /Your (?:District )?Councillors/}
   
   council_info_text = (doc/"table#repstable tr:nth-of-type(1) td:nth-of-type(#{council_col}) p").inner_html
   
-  council_info = council_info_text.match(/Your ([0-9]) (.*) Councillors represent you on the (.*) Council./)
+  council_info = council_info_text.match(/Your ([0-9]) (.*?) (?:District )?Councillors represent you on (?:the )?(.*?) (?:District )?Council./)
   
   @council = council_info[3]
   @ward = council_info[2]
